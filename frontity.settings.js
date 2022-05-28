208aa02a1d232cfefe374b20afec0108ced01f09
MKhalifah <moh.khalifeh95@gmail.com>
2022-05-27 23:19:40 -0700

    Improve BS, add BASE_URL

--- frontity.settings.js
+++ frontity.settings.js
@@ -1,31 +1,31 @@
 const settings = {
-  "name": "lim-site",
-  "state": {
-    "frontity": {
-      "url": "https://staging.lookinmena.com",
-      "title": "LIM 2022",
-      "description": "LIM React"
-    }
+  name: "lim-site",
+  state: {
+    frontity: {
+      url: process.env.BASE_URL,
+      title: "LIM 2022",
+      description: "LIM React",
+    },
   },
-  "packages": [
+  packages: [
     {
-      "name": "lim-theme",
+      name: "lim-theme",
     },
     {
-      "name": "@frontity/wp-source",
-      "state": {
-        "source": {
-          "url": "https://staging.lookinmena.com",
-          "params": {
-            "per_page": 20,
-            "type": ["post", "page", "category"]
-          }
-        }
-      }
+      name: "@frontity/wp-source",
+      state: {
+        source: {
+          url: process.env.BASE_URL,
+          params: {
+            per_page: 20,
+            type: ["post", "page", "category"],
+          },
+        },
+      },
     },
     "@frontity/tiny-router",
-    "@frontity/html2react"
-  ]
+    "@frontity/html2react",
+  ],
 };
 
 export default settings;
