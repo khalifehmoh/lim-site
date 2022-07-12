55b711272d5f4e68d19c5c7ae39019717b8fadcd
khalifehmoh <mkhalifah@atypon.com>
2022-07-12 22:57:36 +0300

    update the settings to read from the base_url

--- frontity.settings.js
+++ frontity.settings.js
@@ -1,13 +1,14 @@
 const getBaseUrl = () => {
   let BASE_URL = "https://staging.lookinmena.com";
   // Set the base url
+  // @todo: update the links once the site is ready
   if (process.env && process.env.NODE_ENV) {
     if (process.env.NODE_ENV === "production") {
-      BASE_URL = "https://lookinmena.com";
+      BASE_URL = "https://staging.lookinmena.com";
     } else if (process.env.NODE_ENV === "staging") {
       BASE_URL = "https://staging.lookinmena.com";
     } else if (process.env.NODE_ENV === "development") {
-      BASE_URL = "https://dev.lookinmena.com";
+      BASE_URL = "https://staging.lookinmena.com";
     }
   }
 
