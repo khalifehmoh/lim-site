2901384ce2ba5427f7e1f7ee456175fa4254666f
khalifehmoh <moh.khalifeh95@gmail.com>
2022-07-12 23:03:58 +0300

    update package.json

--- frontity.settings.js
+++ frontity.settings.js
@@ -1,10 +1,21 @@
-console.log(process.env.BASE_URL);
+console.log(process.env.NODE_ENV);
+let BASE_URL = "https://staging.lookinmena.com";
+// Set the base url
+if (process.env && process.env.NODE_ENV) {
+  if (process.env.NODE_ENV === "production") {
+    BASE_URL = "https://lookinmena.com";
+  } else if (process.env.NODE_ENV === "staging") {
+    BASE_URL = "https://staging.lookinmena.com";
+  } else if (process.env.NODE_ENV === "development") {
+    BASE_URL = "https://dev.lookinmena.com";
+  }
+}
 
 const settings = {
   name: "lim-site",
   state: {
     frontity: {
-      url: process.env.BASE_URL,
+      url: BASE_URL,
       title: "LIM 2022",
       description: "LIM React",
     },
@@ -17,7 +28,7 @@ const settings = {
       name: "@frontity/wp-source",
       state: {
         source: {
-          url: process.env.BASE_URL,
+          url: BASE_URL,
           params: {
             per_page: 20,
             type: ["post", "page", "category"],
