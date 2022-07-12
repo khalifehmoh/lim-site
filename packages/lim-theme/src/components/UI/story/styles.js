0ed1015b99cf74cda5ec389bc6d607e6cc9bfdda
khalifehmoh <mkhalifah@atypon.com>
2022-07-12 22:57:37 +0300

    lazyload images

--- packages/lim-theme/src/components/UI/story/styles.js
+++ packages/lim-theme/src/components/UI/story/styles.js
@@ -1,5 +1,11 @@
 import { styled } from "frontity";
 
-const StyledStory = styled.div``;
+const StyledStory = styled.div`
+    img {
+        width: 100%;
+        height: auto;
+        aspect-ratio: attr(width)/attr(height);
+    }
+`;
 
 export default StyledStory;
